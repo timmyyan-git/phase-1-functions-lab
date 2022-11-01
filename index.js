1fb580deee06827301cb1d1ce307b9942bc83291
Timmy Yan <timmyyan2000@gmail.com>
2022-11-01 00:13:00 -0400

    commit tree d8f48d0bbbc308cccda25907317973ebfe961c8d

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    //returns the number of blocks given a value
+    return Math.abs(42 - block);
+}
+
+function distanceFromHqInFeet(someValue) {
+    const distance = distanceFromHqInBlocks(someValue);
+    // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+    // the return value of distanceFromHqInBlocks can then be used to calculate feet
+    return distance * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    //returns the number of feet traveled
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+    const distance = distanceTravelledInFeet(start, destination);
+
+    if (distance < 400) {
+    return 0;
+    } else if (distance < 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance < 2500) {
+    return 25;
+    } else {
+    return "cannot travel that far";
+    }
+}
+
